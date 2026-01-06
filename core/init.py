e326c78313900a97658bfdb89c11569552c09eb4
trianaq765-cmd <trianaq765@gmail.com>
2026-01-06 10:53:02 +0700

    Create init.py

---
+++ core/init.py
@@ -0,0 +1,15 @@
+"""
+Core Engines - AI, Excel, File handling
+"""
+
+from .ai_engine import AIEngine
+from .excel_engine import ExcelEngine
+from .file_handler import FileHandler
+from .formula_library import FormulaLibrary
+
+__all__ = [
+    "AIEngine",
+    "ExcelEngine",
+    "FileHandler",
+    "FormulaLibrary",
+]
