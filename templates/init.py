37c4869c610d58b95d17e737749df6351fe4d350
trianaq765-cmd <trianaq765@gmail.com>
2026-01-06 11:15:27 +0700

    Create init.py

---
+++ templates/init.py
@@ -0,0 +1,11 @@
+"""
+Templates - Excel and Document templates
+"""
+
+from .excel import ExcelTemplates
+from .documents import DocumentTemplates
+
+__all__ = [
+    "ExcelTemplates",
+    "DocumentTemplates",
+]
